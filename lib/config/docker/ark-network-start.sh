1ac4f454e1169cc53bfd9b38294675fa4a3f7595
Air1 <erwann@ark.io>
2018-10-04 15:47:02 +0400

    chore: trying to understand why node app is not launched on circleci

--- lib/config/docker/ark-network-start.sh
+++ lib/config/docker/ark-network-start.sh
@@ -1,4 +1,5 @@
 echo "Starting ark --network-start" >> output.log
 ls -l ./ >> output.log
+node --version >> output.log
 node ark.js >> output.log
 echo "Started ark --network-start" >> output.log
--- lib/config/docker/ark.sh
+++ lib/config/docker/ark.sh
@@ -1,4 +1,5 @@
 echo "Starting ark" >> output.log
 ls -l ./ >> output.log
+node --version >> output.log
 node ark.js >> output.log
 echo "Started ark" >> output.log

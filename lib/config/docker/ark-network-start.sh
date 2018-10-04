22968d886b07d579f75ecd6e6084149f1b8aa220
Air1 <erwann@ark.io>
2018-10-04 16:06:42 +0400

    chore: redirect error output to file and show it on circleci

--- lib/config/docker/ark-network-start.sh
+++ lib/config/docker/ark-network-start.sh
@@ -1,5 +1,5 @@
 echo "Starting ark --network-start" >> output.log
 ls -l ./ >> output.log
 node --version >> output.log
-node ark.js >> output.log
+node ark.js >> output.log 2> errors.log
 echo "Started ark --network-start" >> output.log
--- lib/config/docker/ark.sh
+++ lib/config/docker/ark.sh
@@ -1,5 +1,5 @@
 echo "Starting ark" >> output.log
 ls -l ./ >> output.log
 node --version >> output.log
-node ark.js >> output.log
+node ark.js >> output.log 2> errors.log
 echo "Started ark" >> output.log

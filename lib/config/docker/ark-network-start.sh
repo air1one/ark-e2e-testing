fa1d02fd114c9a3cb457b5237a49ef9994b3491e
Air1 <erwann@ark.io>
2018-10-04 11:45:28 +0400

    feat: WIP e2e testing on travis

--- lib/config/docker/ark-network-start.sh
+++ lib/config/docker/ark-network-start.sh
@@ -1,3 +1,3 @@
 echo "Starting ark --network-start" >> output.log
-ARK_LOG_FILE=ark.log packages/core/bin/ark start --config packages/core/lib/config/e2enet --network e2enet --network-start >> output.log &
+packages/core/bin/ark start --config packages/core/lib/config/e2enet --network e2enet --network-start >> output.log
 echo "Started ark --network-start" >> output.log

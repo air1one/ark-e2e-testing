75abe99e6ac9463f9da6bce9c8fb6fcd1664c171
Air1 <erwann@ark.io>
2018-10-19 15:19:02 +0400

    chore: debug circleci node init

--- lib/config/docker/entrypoint.sh
+++ lib/config/docker/entrypoint.sh
@@ -4,6 +4,7 @@ cd /ark-core
 rm -rf node_modules package-lock.json > /dev/null 2>&1
 rm -rf packages/core/node_modules packages/core/package-lock.json 2>&1
 npm --quiet install lerna -g && npm --quiet install -g nodemon
+touch lerna.before
 lerna bootstrap
 
 touch lerna.ok
